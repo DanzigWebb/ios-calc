691eb502345fcc6997ffac3f95690bb5feb58b66
Alexandr <valverin95@gmail.com>
2020-11-30 16:37:13 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Александр on 30.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
