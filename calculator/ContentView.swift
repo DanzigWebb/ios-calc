17be2acb250015347a951f26848cb7aa46d98fdc
Alexandr <valverin95@gmail.com>
2020-11-30 17:31:28 +0300

    feat: верстка основных элементов

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["C", "+", "%", "/"],
+        ["7", "8", "9", "x"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "3", "="]
+    ]
+    
+    let spacingGap: CGFloat = 10.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom) {
+            Color.black.ignoresSafeArea(.all)
+            
+            VStack {
+                
+                HStack {
+                    Spacer()
+                    Text("0")
+                        .foregroundColor(.white)
+                        .font(.system(size: 64))
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { line in
+                    HStack(spacing: self.spacingGap) {
+                        ForEach(line, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .foregroundColor(Color.white)
+                                .frame(width: self.btnSize(), height: self.btnSize())
+                                .background(Color.yellow)
+                                .cornerRadius(self.btnSize())
+                        }.padding(.bottom, self.spacingGap)
+                    }
+                }
+            }
+        }
+    }
+    
+    func btnSize() -> CGFloat {
+        return (UIScreen.main.bounds.width - spacingGap * 5) / 4
     }
 }
 
