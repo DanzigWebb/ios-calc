f474ae749813abc552e5394d2b765f9ffb21f16c
Alexandr <valverin95@gmail.com>
2020-12-01 22:23:34 +0300

    feat: определение действия кнопок исходя из символа

--- calculator/CalcBtnsStruct.swift
+++ calculator/CalcBtnsStruct.swift
@@ -9,13 +9,42 @@ import Foundation
 
 struct CalcButton: Identifiable {
   var id = UUID()
+  
   var type: String
   var char: String
+  var effect = "default"
+  
+  init(type: String, char: String) {
+    self.type = type
+    self.char = char
+    self.effect = self.setEffectByChar(char: char)
+  }
+  
+  func setEffectByChar(char: String) -> String {
+    switch char {
+    case "CA":
+      return "reset"
+    case "+":
+      return "addition"
+    case "%":
+      return "percent"
+    case "/":
+      return "division"
+    case "x":
+      return "multiply"
+    case "-":
+      return "subtraction"
+    case "=":
+      return "result"
+    default:
+      return "default"
+    }
+  }
 }
 
 let CalcButtons: [[CalcButton]] = [
   [
-    CalcButton(type: "action", char: "C"),
+    CalcButton(type: "action", char: "CA"),
     CalcButton(type: "action", char: "+"),
     CalcButton(type: "action", char: "%"),
     CalcButton(type: "action", char: "/"),
@@ -40,7 +69,6 @@ let CalcButtons: [[CalcButton]] = [
   ],
   [
     CalcButton(type: "number", char: "0"),
-    CalcButton(type: "number", char: "."),
     CalcButton(type: "number", char: "."),
     CalcButton(type: "action", char: "="),
   ]
